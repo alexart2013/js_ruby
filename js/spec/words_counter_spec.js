d3e518b169dbd047b93603eaa7021cb42c65e9e8
Alex Artamonov <wintel@sfw.su>
2016-07-12 16:36:41 +0300

    renamed file and merhod

--- js/spec/words_counter_spec.js
+++ js/spec/words_counter_spec.js
@@ -1,5 +1,5 @@
 describe("Words Counter", function () {
     it('should return the number of words', function () {
-        expect(countWords("word1 word2")).toEqual(2)
+        expect(wordsCounter("word1 word2")).toEqual(2)
     })
 });
