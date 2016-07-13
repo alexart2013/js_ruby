73214e0ec0d0d605f64a5977bd64954e08f77d64
Alex Artamonov <wintel@sfw.su>
2016-07-13 16:37:16 +0300

    added 6 tests

--- js/spec/words_counter_spec.js
+++ js/spec/words_counter_spec.js
@@ -1,5 +1,26 @@
 describe("Words Counter", function () {
-    it('should return the number of words', function () {
+    it('should returns 2', function () {
         expect(wordsCounter("word1 word2")).toEqual(2)
+    })
+
+    it('should returns 3', function () {
+        expect(wordsCounter("word1 word2 word3")).toEqual(3)
+    })
+
+    it('should returns 2', function () {
+        expect(wordsCounter(" word1 word2")).toEqual(2)
+    })
+
+    it('should returns 4',function () {
+        expect(wordsCounter("word1 word2 3 word4")).toEqual(4)
+
+    })
+
+    it('should returns 1',function () {
+        expect(wordsCounter("word1-word2")).toEqual(1)
+    })
+
+    it('should returns 2',function () {
+        expect(wordsCounter("  word1  word2")).toEqual(2)
     })
 });
--- ruby/spec/words_counter_v3_spec.rb
+++ ruby/spec/words_counter_v3_spec.rb
@@ -1,6 +1,26 @@
 require '../words_counter_v3.rb'
 describe WordsCounter do
-  it 'returns 2 at'  do
-    expect(WordsCounter.count("word word2")).to eq 2
+  it 'should returns 2'  do
+    expect(WordsCounter.count("word1 word2")).to eq 2
+  end
+
+  it 'should return 3' do
+    expect(WordsCounter.count("word1 word2 word3")).to eq 3
+  end
+
+  it 'should return 2' do
+    expect(WordsCounter.count(" word1 word2")).to eq 2
+  end
+
+  it 'should return 1' do
+    expect(WordsCounter.count("word1-word2")).to eq 1
+  end
+
+  it 'should return4' do
+    expect(WordsCounter.count("word1 word2 3 4")).to eq 4
+  end
+
+  it 'should return 2' do
+    expect(WordsCounter.count("  word1  word2")).to eq 2
   end
 end
